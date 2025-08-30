fcd92c3465b4b767842f7096e78b67de8f5c1f62
Nikhil Nath Jha <jhanikhil19122007@gmail.com>
2025-08-30 10:40:30 +0530

    Finalised Edge Detection algorithm, renamed file, cleaned up code.
    Was using edge detected image to find out contours previously, changed to just a blurred thresholded image and the result improved by a lot, also added a max area limit and min area limit to prevent detection of contours that are too big or too small. Cleaned up the code, will use this file as a module and export the final_shape_list list to use in the main program.

---
+++ shapeDetector.py
@@ -0,0 +1,71 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('./uas images/1.png')
+
+# Image Preprocessing
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+blur = cv2.GaussianBlur(gray, (3, 3), 0)
+
+ret, thresh = cv2.threshold(blur, 130, 225, cv2.THRESH_BINARY)
+
+cv2.imshow("thresh", thresh)
+
+contours, hierarchies = cv2.findContours(
+    thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+
+# Avoid Detection of whole photo frame as a separate contour by setting a max threshold and also a minimum area threshold
+max_area_limit = img.shape[0] * img.shape[1]*0.4
+min_area_limit = 25
+
+# print(f'{len(contours)} contours found!!!')
+
+# print(contours)
+
+final_shape_list = []
+
+# drawing overlay on the orignal image to easily test shape detection
+for i, contour in enumerate(contours):
+    # if i == 0:
+    #     continue
+
+    area = cv2.contourArea(contour)
+
+    if area < min_area_limit:
+        continue
+
+    if area > max_area_limit:
+        continue
+
+    final_shape_list.append(contour)
+
+    epsilon = 0.03*cv2.arcLength(contour, True)
+    approx = cv2.approxPolyDP(contour, epsilon, True)
+
+    cv2.drawContours(img, contour, -1, (0, 0, 255), 2)
+
+    x, y, w, h = cv2.boundingRect(approx)
+    x_mid = int(x + w/2)
+    y_mid = int(y + h/2)
+
+    coords = (x_mid, y_mid)
+    color = (0, 0, 0)
+    font = cv2.FONT_HERSHEY_DUPLEX
+
+    if len(approx) == 3:
+        cv2.putText(img, "Triangle", coords, font, 1, color, 1)
+    elif len(approx) == 4:
+        cv2.putText(img, "square", coords, font, 1, color, 1)
+    elif len(approx) == 10:
+        cv2.putText(img, "star", coords, font, 1, color, 1)
+    else:
+        cv2.putText(img, "Circle", coords, font, 1, color, 1)
+
+# Final Number of shapes
+print(len(final_shape_list))
+
+cv2.imshow('og', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
